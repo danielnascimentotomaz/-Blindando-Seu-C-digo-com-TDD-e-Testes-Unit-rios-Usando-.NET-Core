21de1003b53fdac38db691c5c96f05d7440526a1
danielnascimentotomaz <danielsabio1000@hotmail.com>
2023-11-03 14:19:45 -0300

    Criação da Projeto

---
+++ Calculadora/CalculadoraInpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class CalculadoraInpl
+    {
+        public int somar(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ CalculadoraTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Calculadora;
+
+namespace CalculadoraTeste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            CalculadoraInpl calculadora = new CalculadoraInpl();
+
+        }
+    }
+}
